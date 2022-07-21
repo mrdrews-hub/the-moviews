5e8cf605218bb439c299a2c4f3265b1ce016db20
mrdrews-hub <yancuh223@gmail.com>
2022-07-21 11:45:34 +0700

    fix manifest

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,59 @@ export default defineConfig({
       injectRegister: 'auto',
       strategies: 'generateSW',
       registerType: 'autoUpdate',
+      manifest: {
+        "name": "the-moviews-movie-catalog",
+        "short_name": "the-moviews",
+        "theme_color": "#2a2d30",
+        "background_color": "#2a2d30",
+        "display": "standalone",
+        "orientation": "portrait",
+        "scope": "/",
+        "description": "Free Movie Catalog for you",
+        "start_url": "/",
+        icons: [
+          {
+            "src": "images/icons/icon-72x72.png",
+            "sizes": "72x72",
+            "type": "image/png"
+          },
+          {
+            "src": "images/icons/icon-96x96.png",
+            "sizes": "96x96",
+            "type": "image/png"
+          },
+          {
+            "src": "images/icons/icon-128x128.png",
+            "sizes": "128x128",
+            "type": "image/png"
+          },
+          {
+            "src": "images/icons/icon-144x144.png",
+            "sizes": "144x144",
+            "type": "image/png"
+          },
+          {
+            "src": "images/icons/icon-152x152.png",
+            "sizes": "152x152",
+            "type": "image/png"
+          },
+          {
+            "src": "images/icons/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "images/icons/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+            "src": "images/icons/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ]
+      },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
         globIgnores: [
@@ -31,8 +84,6 @@ export default defineConfig({
           }
         ],
         sourcemap: true,
-        clientsClaim: true,
-        skipWaiting: true
       }
     })
   ],
